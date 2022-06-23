f609a2f5547fc0cea573206e1cb4a0ecb6529a05
Manickaraj K M <manickgm@amazon.com>
2022-06-23 21:34:30 +0530

    second

---
+++ pages/api/posts.js
@@ -0,0 +1,30 @@
+import Cors from 'cors';
+
+const corsConfig = {
+    "origin": "https://main.d3mecxrojhc6ov.amplifyapp.com/",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+    };
+const cors = Cors(corsConfig);
+
+function runMiddleware(req, res, fn) {
+return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+    if (result instanceof Error) {
+        return reject(result)
+    }
+
+    return resolve(result)
+    })
+})
+}
+
+export default async function handler(req, res) {
+
+  await runMiddleware(req, res, cors)
+
+  res.status(200).json({ name: 'John Doe' })
+}
+
+
